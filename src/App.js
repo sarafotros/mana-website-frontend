1026f700490509da0a3c0337f66517a1b18cc48b
sarafotros <sarafotros@gmail.com>
2020-03-02 12:02:52 +0000

    rendering users

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const userUrl = "http://localhost:3000/users"
+
+class App extends React.Component {
+
+  state = {
+    users: []
+  }
+
+  componentDidMount() {
+    fetch(userUrl)
+      .then(resp => resp.json())
+      .then(data => this.setState({ users: data }))
+  }
+  
+  render() {
+
+    return (
+
+      <div className="App">
+        <h1> {this.state.users.map(user => user.name)} </h1>
+      
+      </div>
+    );
+  }
 }
 
 export default App;
