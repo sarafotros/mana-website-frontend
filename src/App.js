7435e59331e6471eae9563d1f25d3e584f7eb13f
rmiri <re_miriuk@hotmail.com>
2020-03-05 19:47:45 +0000

    css stuff

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Welcome from './containers/Welcome';
 import { BrowserRouter as Router} from 'react-router-dom'
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 class App extends React.Component {
