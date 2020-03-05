abf06c568f17361e09b089e7c721f139a0aeb8f5
rmiri <re_miriuk@hotmail.com>
2020-03-05 20:57:51 +0000

    products in process

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,11 +4,14 @@ import { Link } from "react-router-dom"
 const Product = ({ product }) => {
 
     return (
+    
+        <div class="productItem">
+            <Link to={`products/${product.id}` }>
+            <h2>{product.name}</h2>
+            <img src={product.image} alt={product.name}></img>
+            </Link>
         
-        <Link to={`products/${product.id}`}>
-        <h2>{product.name}</h2>
-        <img src={product.image} alt={product.name}></img>
-        </Link>
+        </div>
     )
 }
 export default Product
